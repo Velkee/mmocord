4c08742238151b063de4c846d4263e7922ea168f
Fenic <89312879+Velkee@users.noreply.github.com>
2022-02-16 14:30:57 +0100

    Replacing files, added avatar command

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,6 @@
 module.exports = {
   root: true,
   extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  env: ['node: true', 'es6: true'],
+  parserOptions: ['ecmaVersion: 2021'],
 };
